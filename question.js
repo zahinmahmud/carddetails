0e6e5a4017514bc44168d3d90398c778adf8400d
zahinmahmud <iamdiptoboy@gmail.com>
2026-07-13 13:20:33 +0600

    add question

---
+++ question.js
@@ -0,0 +1,37 @@
+const questions = [
+  {
+    question1: "What is the brand of car?"
+  },
+  {
+    question2: "What is the model of the car?"
+  },
+  {
+   question3:"What is the cc of the car"
+  },
+  {
+    question5:`What is the fuel type of the cart  ? Like as - 
+    1) petrol  2) octate 3)Hybrid` 
+
+  },
+  {
+question6:"How many kilometer the vechile is running ?"
+  },
+  {
+    question7:"Which year manufactue and  registration done"
+  },
+  {
+question:"Which package of the car ?"
+  },
+  {
+question9:"How many seats in the cars ?"
+  },
+  {
+question10: " What is the fuel efficiency of the car ?"
+  },
+  {
+question11:"What is the body type of the car ?"
+  },
+{
+    question12:"Has the car ever been involved in an accident?"
+}
+];
