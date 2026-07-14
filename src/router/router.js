ecf85514e613214b12e1b0605a10c72544bc862a
zahinmahmud <iamdiptoboy@gmail.com>
2026-07-14 11:53:54 +0600

    new file chane

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Question from "../components/Question.vue";
 import App from "../App.vue";
@@ -7,7 +7,7 @@ import Detailsoproduct from "../components/Detailsoproduct.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
